e46dd44ca0fdae400b47bc2e3c231b8ca2d05051
Hanyul Park <yoll0765@gmail.com>
2022-05-09 20:06:34 +0900

    build: create electron project

---
+++ electron/index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadFile("main.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+});
+
+// https://d4emon.tistory.com/155
