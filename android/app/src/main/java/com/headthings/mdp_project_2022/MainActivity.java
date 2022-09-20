bb1eaec3e35fcf3a2417cbee748788000896a32e
Hanyul Park <yoll0765@gmail.com>
2022-09-20 15:18:41 +0900

    Add expandablebottombar to activity_main

--- android/app/src/main/java/com/headthings/mdp_project_2022/MainActivity.java
+++ android/app/src/main/java/com/headthings/mdp_project_2022/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+import github.com.st235.lib_expandablebottombar.Menu;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ExpandableBottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomBar = findViewById(R.id.bottom_bar);
     }
+
 }
