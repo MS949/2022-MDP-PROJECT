3e192e2e0e47eb4f196f5bb68398f62a1a57a95c
Hanyul Park <yoll0765@gmail.com>
2022-09-20 16:56:35 +0900

    Add expandablebottombar event

--- android/app/src/main/java/com/headthings/mdp_project_2022/MainActivity.java
+++ android/app/src/main/java/com/headthings/mdp_project_2022/MainActivity.java
@@ -1,22 +1,35 @@
 package com.headthings.mdp_project_2022;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
-import github.com.st235.lib_expandablebottombar.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ExpandableBottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomBar = findViewById(R.id.bottom_bar);
-    }
+        ExpandableBottomBar bottomBar = findViewById(R.id.bottom_bar);
 
+        bottomBar.setOnItemSelectedListener((view, menuItem, aBoolean) -> {
+            switch (menuItem.getId()) {
+                case R.id.setting:
+                    Log.d("MainActivity", "setting press");
+                    break;
+                case R.id.home:
+                    Log.d("MainActivity", "home press");
+                    break;
+                case R.id.other:
+                    Log.d("MainActivity", "other press");
+                    break;
+            }
+
+            return null;
+        });
+    }
 }
