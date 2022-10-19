60cf7aad5773f3c66920887afeb51d5d38ab32aa
Hanyul Park <yoll0765@gmail.com>
2022-10-19 19:52:18 +0900

    Implement OneClickView

--- android/app/src/main/java/com/headthings/mdp_project_2022/OneClickView.java
+++ android/app/src/main/java/com/headthings/mdp_project_2022/OneClickView.java
@@ -5,15 +5,20 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class OneClickView extends LinearLayout {
+public class OneClickView extends LinearLayout implements CompoundButton.OnCheckedChangeListener {
 
-    LinearLayout bg;
-    ImageView symbol;
-    TextView text;
+    private LinearLayout bg;
+    private ImageView symbol;
+    private TextView text;
+    private Switch power;
+
+    private CompoundButton.OnCheckedChangeListener mOnCheckedChangeListener;
 
     public OneClickView(Context context) {
         super(context);
@@ -42,53 +47,75 @@ public class OneClickView extends LinearLayout {
         bg = findViewById(R.id.bg);
         symbol = findViewById(R.id.symbol);
         text = findViewById(R.id.text);
+        power = findViewById(R.id.power);
+
+        power.setOnCheckedChangeListener(this);
+    }
+
+    public interface OnCheckedChangeListener {
+        void onCheckedChanged(CompoundButton buttonView, boolean isChecked);
+    }
+
+    public void setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener mListener) {
+        mOnCheckedChangeListener = mListener;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        mOnCheckedChangeListener.onCheckedChanged(compoundButton, b);
     }
 
     private void getAttrs(AttributeSet attrs) {
-        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.LoginButton);
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.OneClickView);
         setTypeArray(typedArray);
     }
 
 
     private void getAttrs(AttributeSet attrs, int defStyle) {
-        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.LoginButton, defStyle, 0);
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.OneClickView, defStyle, 0);
         setTypeArray(typedArray);
     }
 
     private void setTypeArray(TypedArray typedArray) {
-        int bg_resID = typedArray.getResourceId(R.styleable.LoginButton_bg, R.drawable.oval_black);
+        int bg_resID = typedArray.getResourceId(R.styleable.OneClickView_bg, R.drawable.shape_oval_black);
         bg.setBackgroundResource(bg_resID);
 
-        int symbol_resID = typedArray.getResourceId(R.styleable.LoginButton_symbol, R.drawable.ic_camera_outdoor);
+        int symbol_resID = typedArray.getResourceId(R.styleable.OneClickView_symbol, 0);
         symbol.setImageResource(symbol_resID);
 
-        int textColor = typedArray.getColor(R.styleable.LoginButton_textColor, 0);
+        int textColor = typedArray.getColor(R.styleable.OneClickView_textColor, 0);
         text.setTextColor(textColor);
 
-        String text_string = typedArray.getString(R.styleable.LoginButton_text);
+        String text_string = typedArray.getString(R.styleable.OneClickView_text);
         text.setText(text_string);
+
+        boolean isPower = typedArray.getBoolean(R.styleable.OneClickView_isPower, false);
+        power.setChecked(isPower);
 
         typedArray.recycle();
     }
 
+    public void setPower(boolean isPower) {
+        power.setChecked(isPower);
+    }
 
-    void setBg(int bg_resID) {
+    public void setBg(int bg_resID) {
         bg.setBackgroundResource(bg_resID);
     }
 
-    void setSymbol(int symbol_resID) {
+    public void setSymbol(int symbol_resID) {
         symbol.setImageResource(symbol_resID);
     }
 
-    void setTextColor(int color) {
+    public void setTextColor(int color) {
         text.setTextColor(color);
     }
 
-    void setText(String text_string) {
+    public void setText(String text_string) {
         text.setText(text_string);
     }
 
-    void setText(int text_resID) {
+    public void setText(int text_resID) {
         text.setText(text_resID);
     }
 }
--- android/app/src/main/java/com/headthings/mdp_project_2022/mainFragment/HomeFragment.java
+++ android/app/src/main/java/com/headthings/mdp_project_2022/mainFragment/HomeFragment.java
@@ -4,58 +4,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.headthings.mdp_project_2022.OneClickView;
 import com.headthings.mdp_project_2022.R;
 import com.headthings.mdp_project_2022.SplashActivity;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 
-public class HomeFragment extends Fragment implements CompoundButton.OnCheckedChangeListener {
+public class HomeFragment extends Fragment {
+
+    @BindView(R.id.curtain)
+    OneClickView curtain;
+
+    @BindView(R.id.camera)
+    OneClickView camera;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, view);
+
+        curtain.setOnCheckedChangeListener((compoundButton, b) -> {
+            try {
+                if (b) {
+                    SplashActivity.mqttClient.publish("test/test1", "on".getBytes(), 0, false);
+                } else {
+                    SplashActivity.mqttClient.publish("test/test1", "off".getBytes(), 0, false);
+                }
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        });
+        camera.setOnCheckedChangeListener((compoundButton, b) -> {
+            try {
+                if (b) {
+                    SplashActivity.mqttClient.publish("test/test2", "on".getBytes(), 0, false);
+                } else {
+                    SplashActivity.mqttClient.publish("test/test2", "off".getBytes(), 0, false);
+                }
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        });
+
         return view;
     }
 
-    @OnCheckedChanged({R.id.switch1, R.id.switch2, R.id.switch3})
-    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        try {
-            switch (compoundButton.getId()) {
-                case R.id.switch1:
-                    if (isChecked) {
-                        SplashActivity.mqttClient.publish("test/test1", "on".getBytes(), 0, false);
-                    } else {
-                        SplashActivity.mqttClient.publish("test/test1", "off".getBytes(), 0, false);
-                    }
-                    break;
-                case R.id.switch2:
-                    if (isChecked) {
-                        SplashActivity.mqttClient.publish("test/test2", "on".getBytes(), 0, false);
-                    } else {
-                        SplashActivity.mqttClient.publish("test/test2", "off".getBytes(), 0, false);
-                    }
-                    break;
-                case R.id.switch3:
-                    if (isChecked) {
-                        SplashActivity.mqttClient.publish("test/test3", "on".getBytes(), 0, false);
-                    } else {
-                        SplashActivity.mqttClient.publish("test/test3", "off".getBytes(), 0, false);
-                    }
-                    break;
-            }
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
 }
